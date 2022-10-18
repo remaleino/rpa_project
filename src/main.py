bc3af617c2516ce3ea4b7e5dbd22d42943f7d68d
remaleino <raimond.leino@gmail.com>
2022-10-18 16:59:43 +0300

    update 1.1

--- src/main.py
+++ src/main.py
@@ -1,12 +1,26 @@
-import requests, sys, argparse
+import csv
+import requests
+import sys
+import argparse
+import urllib
 
 # Download .csv file
+
+
 def download_csv_file(url):
-    pass
+    urllib.request.urlretrieve(url, "file.csv")
+    data = {}
+    with open('file.csv', 'r') as csvFile:
+        dict_reader = csv.DictReader(csvFile)
+        l = list(dict_reader)
+        data = l
+    return (data)
 
 
-
-# convert .csv to excel
+def csv_to_json_converter():
+    url = "https://backendforrobot.herokuapp.com/api/schedule"
+    data = download_csv_file(url)
+    print(data)
 
 
 # data analyysi
@@ -14,12 +28,14 @@ def download_csv_file(url):
 
 # robot framework
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Description of your program')
-    parser.add_argument('-start','--start-date', help='Description for foo argument', required=True)
-    parser.add_argument('-end','--end-date', help='Description for bar argument', required=False)
-    parser.add_argument('-s','--supervisor', help='Description for bar argument', required=False)
+    parser.add_argument('-start', '--start-date',
+                        help='Description for foo argument', required=True)
+    parser.add_argument('-end', '--end-date',
+                        help='Description for bar argument', required=False)
+    parser.add_argument('-s', '--supervisor',
+                        help='Description for bar argument', required=False)
     args = vars(parser.parse_args())
 
     start_date = args['start_date']

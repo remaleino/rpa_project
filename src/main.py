52332e926f4545caa1e6be28980de2b411926ede
jevgenix <jekka9803@gmail.com>
2022-10-18 16:49:09 +0300

    project setup

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
-import requests, sys, argparse
+import requests, argparse, csv, json, os, robot.api.logger as logger
+from robot.libraries.BuiltIn import BuiltIn
+#import robot.api.logger as logger
+
+
 
 # Download .csv file
 def download_csv_file(url):
     pass
 
+def convert_csv_file_to_json(csv):
+    pass
 
+def iterate_over_json(json):
+    pass
 
 # convert .csv to excel
 
@@ -18,14 +26,15 @@ def download_csv_file(url):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Description of your program')
     parser.add_argument('-start','--start-date', help='Description for foo argument', required=True)
-    parser.add_argument('-end','--end-date', help='Description for bar argument', required=False)
+    parser.add_argument('-end','--end-date', help='Description for bar argument', required=True)
     parser.add_argument('-s','--supervisor', help='Description for bar argument', required=False)
     args = vars(parser.parse_args())
 
-    start_date = args['start_date']
+    start_date = args['start_date'] 
     end_date = args['end_date']
-    supervisor = args['supervisor']
+    supervisor = (args['supervisor'] or None)
 
-    print(start_date)
-    print(end_date)
-    print(supervisor)
+    if supervisor == None:
+        logger.warn("You are missing the supervisor!")
+    
+    url = ''
